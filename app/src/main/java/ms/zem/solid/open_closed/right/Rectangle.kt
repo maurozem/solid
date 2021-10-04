17a97e221a1c237de615952413ad5ca2aa3d2c7f
maurozem <maurozem@brq.com>
2021-10-04 09:22:38 -0300

    solid - correções

--- app/src/main/java/ms/zem/solid/open_closed/right/Rectangle.kt
+++ app/src/main/java/ms/zem/solid/open_closed/right/Rectangle.kt
@@ -3,4 +3,6 @@ package ms.zem.solid.open_closed.right
 data class Rectangle (
     var lenght: Double,
     var height: Double
-)
+) : Shape {
+    override fun getArea(): Double = lenght * height
+}
--- app/src/main/java/ms/zem/solid/open_closed/wrong/Rectangle.kt
+++ app/src/main/java/ms/zem/solid/open_closed/wrong/Rectangle.kt
@@ -1,10 +1,6 @@
 package ms.zem.solid.open_closed.wrong
 
-import ms.zem.solid.open_closed.right.Shape
-
 data class Rectangle (
     var lenght: Double,
     var height: Double
-) : Shape {
-    override fun getArea(): Double = lenght * height
-}
+)
